f27db2a6ef1db4f9105876568e820fb00978defc
parentalclash <parentalclash@gmail.com>
2024-07-16 19:10:33 -0400

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -412,7 +412,7 @@ show_enable_status() {
 }
 
 check_xray_status() {
-    count=$(ps -ef | grep "xray-$release" | grep -v "grep" | wc -l)
+    count=$(ps -ef | grep "xray-${release}" | grep -v "grep" | wc -l)
     if [[ count -ne 0 ]]; then
         return 0
     else
